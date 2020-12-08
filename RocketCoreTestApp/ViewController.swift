62ce14aa2ef80205400eb1dedbd76f5e85e32469
Serj <serj.rubens@gmail.com>
2020-12-08 13:08:59 +0100

    Initial Commit

---
+++ RocketCoreTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RocketCoreTestApp
+//
+//  Created by Serj Rubens on 08.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
